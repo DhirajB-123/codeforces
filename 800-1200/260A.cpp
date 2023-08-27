b85768cc77cbe4a1455876f3d2c057ac315e2a31
DhirajB-123 <dhirajbomma@gmail.com>
2023-08-26 22:16:16 -0700

    gonna work thru a2oj ladder

---
+++ 800-1200/260A.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll a, b, n;
+  cin >> a >> b >> n;
+  if (a % b == 0){
+    cout << a;
+    for (int i = 0; i < n; i++){
+      cout << 0;
+    }
+    return 0;
+  }
+  a *= 10;
+  for (int i = 0; i < 10; i++) {
+    if ((a + i) % b == 0){
+      cout << a + i;
+      for (int i = 1; i < n; i++){
+	cout << 0;
+      }
+      return 0;
+    }
+  }
+  cout << -1;
+}
---
+++ 800-1200/271A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+bool distinct(int val){
+  int digit;
+  set<int> seen;
+  for (int i = 0; i < 4; i++){
+    digit = val % 10; 
+    val = val / 10;
+    seen.insert(digit);
+  }
+  return (seen.size() == 4);
+}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  int year;
+  cin >> year;
+  year++;
+  while (true){
+    if (distinct(year)) {
+      break;
+    }
+    year++;
+  }
+  cout << year;
+}
