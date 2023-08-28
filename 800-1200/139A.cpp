79612fba5ec0f2dd0b58108f079a10c63b130bcf
DhirajB-123 <dhirajbomma@gmail.com>
2023-08-27 20:47:59 -0700

    more ladder qs

---
+++ 800-1200/139A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  ll pages, day, last, total = 0;
+  cin >> pages;
+  vl days;
+  days.push_back(0);
+  f(i, 0, 7){
+    cin >> day;
+    days.pb(day);
+    total += day;
+    if (day != 0){
+      last = i + 1;
+    }
+  }
+  pages = pages % total;
+  if (pages == 0){
+    cout << last;
+    return 0;
+  }
+  for (int i = 1; i < 8; i++) {
+    pages -= days[i];
+    if (pages <= 0){
+      cout << i;
+      return 0;
+    }
+  }
+}
