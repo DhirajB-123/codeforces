15b77efc558b1d65c5b6c983aac2914c2e0c5b77
DhirajB-123 <dhirajbomma@gmail.com>
2023-11-07 16:36:11 -0800

    wow i did great this vc

---
+++ virtual-contests/Round903-Div3/solved/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  TC {
+    int t1, t2;
+    cin >> t1 >> t2;
+
+    string inp, goal;
+    cin >> inp >> goal;
+
+    int res = -1;
+    for (int i = 0; i < 10; i++){
+      if (inp.find(goal) != string::npos){
+	res = i;
+	break;
+      }
+      inp += inp;
+    }
+
+    o(res);
+  }
+}
---
+++ virtual-contests/Round903-Div3/solved/B.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  TC {
+    rv(vals, 3);
+    sort(all(vals));
+    ll tmp;
+    for (int i = 0; i < 3; i++){
+      if (vals[0] == vals[2 + i]) break;
+      tmp = vals[2 + i] - vals[0];
+      vals[2+i] = vals[0];
+      vals.pb(tmp);
+      sort(all(vals));
+    }
+
+    ll size = vals.size();
+    if (vals[0] == vals[size-1]) {
+      cout << "YES" << endl;
+    }
+    else {
+      cout << "NO" << endl;
+    }
+
+  }
+}
