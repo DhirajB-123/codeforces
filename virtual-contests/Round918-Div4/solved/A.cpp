c13bfad5b071dfdcf6a1c5ba9e2fd1b93133331d
zyx-db <dhirajbomma@gmail.com>
2024-01-13 17:34:35 -0800

    virt

---
+++ virtual-contests/Round918-Div4/solved/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  TC {
+    rv(vals, 3);
+    if (vals[0] == vals[1]) {
+      cout << vals[2];
+    }
+    if (vals[1] == vals[2]) {
+      cout << vals[0];
+    }
+    if (vals[0] == vals[2]) {
+      cout << vals[1];
+    }
+    cout << "\n";
+  }
+}
---
+++ virtual-contests/Round918-Div4/solved/D.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <cstring>
+#include <ios>
+#include <unordered_map>
+using namespace std;
+ 
+typedef long long int ll;
+typedef long double ld;
+typedef vector<ll> vl;
+typedef pair<ll, ll> pl;
+typedef vector<pair<ll, ll> > vll;
+#define pb push_back
+#define all(x) (x).begin(), (x).end()
+#define f(i,l,r) for (ll i = l; i < r; ++i)
+#define fd(i,r,l) for (ll i = r; i >= l; --i)
+#define r(x) ll x; cin >> x;
+#define rd(x) ld x; cin >> x;
+#define rs(s) string s; cin >> s;
+#define rc(c) char c; cin >> c;
+#define rv(a,n) vl a(n); f(i, 0, n) cin >> a[i];
+#define rnv(a) r(n); rv(a,n);
+#define pv(a) f(i, 0, a.size()) {cout << a[i] << " ";} cout << endl;
+#define TC ll ntests;cin>>ntests;ll tc=0;goto _restart;_restart:;while(++tc<=ntests)
+#define o(x) {cout<<x<<'\n'; goto _restart;}
+#define ov(a) {pv(a) goto _restart;}
+
+int main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+  TC {
+    ll chars;
+    cin >> chars;
+    bool first = false;
+    char f, s;
+    cin >> f;
+    f(i, 1, chars){
+      cin >> s; 
+      if (vowels.find(s) != vowels.end()){
+	if (first){
+	  cout << ".";
+	}
+	else {
+	  first = true;
+	}
+      }
+      cout << f;
+      f = s;
+    }
+    cout << s;
+    cout << endl;
+  }
+}
